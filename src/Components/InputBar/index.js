3ebfb535cc99dbb523a9b7e56d9d70385ae68a76
notnora <thisisnotnora@gmail.com>
2020-08-28 11:16:00 +0200

    Added an ingredient input

---
+++ src/Components/InputBar/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {TextField} from "@material-ui/core";
+
+export const InputBar = props => {
+
+    const [inputValue, setInputValue] = React.useState("");
+    const handleKeyDown = event => {
+        if (event.key === 'Enter') {
+            let ingredient = event.target.value;
+            console.log('Enter: ', ingredient);
+            props.addIngredient(ingredient);
+            setInputValue(inputValue => []);
+        }
+    };
+    const handleChange = event => {
+        setInputValue(event.target.value);
+    };
+
+    return (
+        <div>
+            <TextField
+                id="inputbar"
+                type="text"
+                onKeyDown={handleKeyDown}
+                onChange={handleChange}
+                value={inputValue}/>
+
+        </div>
+    );
+};
+export default InputBar;
--- src/Components/MainWindow.js
+++ src/Components/MainWindow.js
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {SearchBar} from "./SearchBar";
+import {InputBar} from './InputBar';
+import { Button } from '@material-ui/core';
 
 export const MainWindow = props => {
     /*
         States
      */
     const [searchWord, setSearchWord] = React.useState("");
+    const [ingredientList, setIngredientList] = React.useState([]);
+    useEffect(() => {
+        console.log("New ingredient list: ", ingredientList);
+
+    });
 
     const searchRecipe = word => {
         setSearchWord(word);
         console.log("Searchword: ", word)
     };
+    const addIngredientToList = ingredient => {
+        setIngredientList(ingredients =>[...ingredients, ingredient]);
+        console.log("Added: ", ingredient);
+    };
+    const resetIngredients = () => {
+        setIngredientList(ingredientList => []);
+    };
     return (
-        <SearchBar
-        search = {searchRecipe}/>
+        <div>
+            <SearchBar
+                search = {searchRecipe}/>
+            <InputBar
+                ingredients={ingredientList}
+                addIngredient = {addIngredientToList}/>
+            <Button
+                onClick ={resetIngredients}
+                variant="contained"
+                color="primary">
+                Reset ingredients
+            </Button>
+
+
+        </div>
+
     );
 };
 export default MainWindow;
